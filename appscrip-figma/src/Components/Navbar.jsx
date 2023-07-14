a9fd97dffe50876d95760d34ab3eadc99c4a2119
tanishaa17 <krati1701@gmail.com>
2023-07-14 18:57:42 +0530

    navbar structure created

--- appscrip-figma/src/Components/Navbar.jsx
+++ appscrip-figma/src/Components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
-
+import styles from '../Styles/Navbar.module.css'
 export const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className={module.navDiv}>
+            <div className={styles.logo}>LOGO</div>
+            <div className={styles.content}>
+                <div className={styles.navContent}>
+                    <p>Home</p>
+                    <p>About Us</p>
+                    <p>Our Blog</p>
+                    <p>Support</p>
+                    <p>Affiliate</p>
+                    <p>Program ~</p>
+                </div>
+                <div className={styles.loginDiv}>
+                    <p>Login</p>
+                    <p>Sign up</p>
+                </div>
+            </div>
+        </div>
     )
 }
