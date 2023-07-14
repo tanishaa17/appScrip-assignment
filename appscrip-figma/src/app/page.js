22486e42350921cacf951b8465a067d433225634
tanishaa17 <krati1701@gmail.com>
2023-07-14 18:40:07 +0530

    folder structure created

---
+++ appscrip-figma/src/app/page.js
@@ -0,0 +1,16 @@
+import { Header } from '@/Pages/Header'
+import { Navbar } from '@/Components/Navbar'
+import { Main } from '@/Pages/Main'
+import { Footer } from '@/Components/Footer'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+
+  )
+}
